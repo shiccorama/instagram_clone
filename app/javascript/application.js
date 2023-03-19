39bf5f6ea09ddece647b30429cc233d0e4ff2b8b
shiccorama <shiccorama@gmail.com>
2023-03-19 14:35:28 +0200

    basic_project_with_views

---
+++ app/javascript/application.js
@@ -0,0 +1,24 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "./add_jquery"
+
+import "jquery"
+import "jquery_ujs"
+import "popper"
+import "bootstrap"
+
+
+
+
+
+
+
+//= require rails-ujs
+//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require bootstrap.min
+//= require bootstrap-sprockets
+//= require_tree
